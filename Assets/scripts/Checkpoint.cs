0bff7d53c9d41fc9bb3f4d9bce04ed8ece201fe2
Stachebro12 <bendavis@outlook.com.au>
2023-05-15 16:46:35 +1000

    checkpoint 0.2

--- Assets/scripts/Checkpoint.cs
+++ Assets/scripts/Checkpoint.cs
@@ -6,6 +6,7 @@ public class Checkpoint : MonoBehaviour
 {
     private GameObject stats;
     public Transform Spawn;
+    public int CheckpointNum;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,45 @@ public class Checkpoint : MonoBehaviour
     void OnTriggerEnter2D(Collider2D collision)
     {
         stats.GetComponent<Stats>().NewSpawnPoint(Spawn);
+        if(CheckpointNum == 1)
+        {
+            stats.GetComponent<Stats>().check1();
+        }
+        if (CheckpointNum == 2)
+        {
+            stats.GetComponent<Stats>().check2();
+        }
+        if (CheckpointNum == 3)
+        {
+            stats.GetComponent<Stats>().check3();
+        }
+        if (CheckpointNum == 4)
+        {
+            stats.GetComponent<Stats>().check4();
+        }
+        if (CheckpointNum == 5)
+        {
+            stats.GetComponent<Stats>().check5();
+        }
+        if (CheckpointNum == 6)
+        {
+            stats.GetComponent<Stats>().check6();
+        }
+        if (CheckpointNum == 7)
+        {
+            stats.GetComponent<Stats>().check7();
+        }
+        if (CheckpointNum == 8)
+        {
+            stats.GetComponent<Stats>().check8();
+        }
+        if (CheckpointNum == 9)
+        {
+            stats.GetComponent<Stats>().check9();
+        }
+        if (CheckpointNum == 10)
+        {
+            stats.GetComponent<Stats>().check10();
+        }
     }
 }
--- Assets/scripts/Stats.cs
+++ Assets/scripts/Stats.cs
@@ -5,6 +5,17 @@ using UnityEngine;
 public class Stats : MonoBehaviour {
     public int dateNum;
     public Transform Spawn;
+    public bool checkpoint1 = false;
+    public bool checkpoint2 = false;
+    public bool checkpoint3 = false;
+    public bool checkpoint4 = false;
+    public bool checkpoint5 = false;
+    public bool checkpoint6 = false;
+    public bool checkpoint7 = false;
+    public bool checkpoint8 = false;
+    public bool checkpoint9 = false;
+    public bool checkpoint10 = false;
+    public bool TEWon = false;
 
     void Start() {
         DontDestroyOnLoad(this.gameObject); //This is necessary to ensure the HUD carries between scenes.
@@ -17,5 +28,50 @@ public class Stats : MonoBehaviour {
     public void NewSpawnPoint(Transform SpawnPoint)
     {
         Spawn = SpawnPoint;
+    }
+
+    public void check1()
+    {
+        checkpoint1 = true;
+    }
+    public void check2()
+    {
+        checkpoint2 = true;
+    }
+    public void check3()
+    {
+        checkpoint3 = true;
+    }
+    public void check4()
+    {
+        checkpoint4 = true;
+    }
+    public void check5()
+    {
+        checkpoint5 = true;
+    }
+    public void check6()
+    {
+        checkpoint6 = true;
+    }
+    public void check7()
+    {
+        checkpoint7 = true;
+    }
+    public void check8()
+    {
+        checkpoint8 = true;
+    }
+    public void check9()
+    {
+        checkpoint9 = true;
+    }
+    public void check10()
+    {
+        checkpoint10 = true;
+    }
+    public void TEWin()
+    {
+        TEWon = true;
     }
 }
