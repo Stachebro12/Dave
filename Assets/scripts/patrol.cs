47fe73d7652bfbb9ea97de48d3a03a7977e8fee3
Stachebro12 <bendavis@outlook.com.au>
2024-01-18 16:01:56 +1000

    animation fixes

--- Assets/scripts/patrol.cs
+++ Assets/scripts/patrol.cs
@@ -16,29 +16,32 @@ public class patrol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Transform wp = waypoints[_currentWaypointIndex];
-        if (Vector3.Distance(transform.position, wp.position) < 0.01f)
+        if (gameObject.GetComponent<guard>().stunned == false)
         {
-            _currentWaypointIndex = (_currentWaypointIndex + 1) % waypoints.Length;
-        }
-        else
-        {
-            transform.position = Vector3.MoveTowards(
-                transform.position,
-                wp.position,
-                _speed * Time.deltaTime);
-            transform.LookAt(wp.position);
-            // what direction do I want to look in?
-            Vector3 delta = wp.position - transform.position;
+            Transform wp = waypoints[_currentWaypointIndex];
+            if (Vector3.Distance(transform.position, wp.position) < 0.01f)
+            {
+                _currentWaypointIndex = (_currentWaypointIndex + 1) % waypoints.Length;
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(
+                    transform.position,
+                    wp.position,
+                    _speed * Time.deltaTime);
+                transform.LookAt(wp.position);
+                // what direction do I want to look in?
+                Vector3 delta = wp.position - transform.position;
 
-            // if we're at the target position, don't rotate -- just keep facing current direction (and don't divide by 0)
-            if (delta.x == 0 && delta.y == 0) return;
+                // if we're at the target position, don't rotate -- just keep facing current direction (and don't divide by 0)
+                if (delta.x == 0 && delta.y == 0) return;
 
-            // what is the rotation angle (around the Z axis) ; NB, -90 assumes sprite was drawn facing "UP" for an angle of 0
-            float angle = Mathf.Rad2Deg * Mathf.Atan2(delta.y, delta.x) - 90.0f;
+                // what is the rotation angle (around the Z axis) ; NB, -90 assumes sprite was drawn facing "UP" for an angle of 0
+                float angle = Mathf.Rad2Deg * Mathf.Atan2(delta.y, delta.x) - 90.0f;
 
-            // and set the angle
-            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);	// NB, that's forward in 3d space - z as 1
+                // and set the angle
+                transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);  // NB, that's forward in 3d space - z as 1
+            }
         }
     }
 }
--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -20,7 +20,6 @@ public class playerController : MonoBehaviour
     private GameObject Stats;
     private GameObject pp;
     private Animator Walk;
-    private bool spawned = false;
 
 
     // Start is called before the first frame update
@@ -39,60 +38,53 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (spawned == false)
-        {
-            
-            /*rigidBody.MovePosition(new Vector3(SpawnX, SpawnY, 0));
-            GetComponent<BoxCollider2D>().enabled = false;
-            if (transform.position.x == SpawnX & transform.position.y == SpawnY)
-            {
-                spawned = true;
-                GetComponent<BoxCollider2D>().enabled = true;
-            }*/
-        }
-        if (Input.GetKey(downKey))
-        {
-            rigidBody.MovePosition(transform.position + new Vector3(0, -distY, 0));
-            transform.localRotation = Quaternion.Euler(0, 0, 180);
-            Walk.SetTrigger("move");
-        }
-        if (Input.GetKey(upKey))
-        {
-            rigidBody.MovePosition(transform.position + new Vector3(0, distY, 0));
-            transform.localRotation = Quaternion.Euler(0, 0, 0);
-            Walk.SetTrigger("move");
-        }
-        if (Input.GetKey(rightKey))
-        {
-            rigidBody.MovePosition(transform.position + new Vector3(distX, 0, 0));
-            transform.localRotation = Quaternion.Euler(0, 0, 270);
-            Walk.SetTrigger("move");
-        }
-        if (Input.GetKey(leftKey))
-        {
-            rigidBody.MovePosition(transform.position + new Vector3(-distX, 0, 0));
-            transform.localRotation = Quaternion.Euler(0, 0, 90);
-            Walk.SetTrigger("move");
-        }
         if (Input.GetKey(upKey) && Input.GetKey(rightKey))
         {
             rigidBody.MovePosition(transform.position + new Vector3(distD, distD, 0));
+            Walk.SetBool("walk", true);
         }
-        if (Input.GetKey(upKey) && Input.GetKey(leftKey))
+        else if (Input.GetKey(upKey) && Input.GetKey(leftKey))
         {
             rigidBody.MovePosition(transform.position + new Vector3(-distD, distD, 0));
+            Walk.SetBool("walk", true);
         }
-        if (Input.GetKey(downKey) && Input.GetKey(rightKey))
+        else if (Input.GetKey(downKey) && Input.GetKey(rightKey))
         {
             rigidBody.MovePosition(transform.position + new Vector3(distD, -distD, 0));
+            Walk.SetBool("walk", true);
         }
-        if (Input.GetKey(downKey) && Input.GetKey(leftKey))
+        else if (Input.GetKey(downKey) && Input.GetKey(leftKey))
         {
             rigidBody.MovePosition(transform.position + new Vector3(-distD, -distD, 0));
+            Walk.SetBool("walk", true);
         }
-        if (!Input.anyKey)
+        else if (Input.GetKey(downKey))
         {
-            Walk.SetTrigger("stop");
+            rigidBody.MovePosition(transform.position + new Vector3(0, -distY, 0));
+            transform.localRotation = Quaternion.Euler(0, 0, 180);
+            Walk.SetBool("walk", true);
+        }
+        else if (Input.GetKey(upKey))
+        {
+            rigidBody.MovePosition(transform.position + new Vector3(0, distY, 0));
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+            Walk.SetBool("walk", true);
+        }
+        else if (Input.GetKey(rightKey))
+        {
+            rigidBody.MovePosition(transform.position + new Vector3(distX, 0, 0));
+            transform.localRotation = Quaternion.Euler(0, 0, 270);
+            Walk.SetBool("walk", true);
+        }
+        else if (Input.GetKey(leftKey))
+        {
+            rigidBody.MovePosition(transform.position + new Vector3(-distX, 0, 0));
+            transform.localRotation = Quaternion.Euler(0, 0, 90);
+            Walk.SetBool("walk", true);
+        }
+        else
+        {
+            Walk.SetBool("walk", false);
         }
     }
 }
